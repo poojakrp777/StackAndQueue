4109076f822dab0652c50e879e640cc7c73fa297
poojakrp777 <poojakrp777@gmail.com>
2022-10-25 22:23:30 +0530

    create stack

---
+++ LinkedList.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace StackAndQueue
+{
+    internal class LinkedList
+    {
+        public Node top;
+        public LinkedList()
+        {
+            this.top = null;
+        }
+        internal void push(int value)
+        {
+            Node node = new Node(value);
+            if (this.top == null)
+            {
+                node.next = null;
+            }
+            else
+            {
+                node.next = this.top;
+            }
+            this.top = node;
+            Console.WriteLine(value);
+        }
+        internal void Display()
+        {
+            Node temp = this.top;
+            while (temp != null)
+            {
+                Console.WriteLine("Inserted into the stack Successfully = " + temp.data);
+                temp = temp.next;
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackAndQueue
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Stacks and Queues Programs");
+
+            LinkedList stackUsingLinkList = new LinkedList();
+            stackUsingLinkList.push(70);
+            stackUsingLinkList.push(30);
+            stackUsingLinkList.push(56);
+            stackUsingLinkList.Display();
+        }
+    }
+}
